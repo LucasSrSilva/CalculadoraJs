3ba8c8111ebd346907d137fd8979e86be35b7f4a
LucasSrSilva <lucas.srs.2022@gmail.com>
2024-02-13 17:52:17 -0300

    v2

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ O objetivo desse projeto é criar uma calculadora funcional utilizando HTML, CSS
 - calcular os dados inseridos na interface utilizando botões funcionais.
 - apagar números e funções de calculo com o botão "apagar".
 - limpar a tela utilizando o botão "limpar"
+- adicionar um histórico de resultados
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function insert(num){
+    var resultado = document.getElementById('resultado').innerHTML;
+    if (('+-*/').includes(resultado.slice(-1)) && '+-*/'.includes(num)) {
+        return;
+    }
     document.getElementById('resultado').innerHTML += num;
 }
 
@@ -12,7 +16,29 @@ function apagar(){
 
 function calcular(){
     var resultado = document.getElementById('resultado').innerHTML;
+
     if(resultado){
-        document.getElementById('resultado').innerHTML = eval(resultado);
+
+        var resultadoCalculado = eval(resultado);
+
+        document.getElementById('resultado').innerHTML = resultadoCalculado;
+
+        var historico = document.getElementById('historico')
+        var novoItem = document.createElement('li')
+
+        novoItem.textContent = resultado + '=' + resultadoCalculado;
+        historico.appendChild(novoItem);
     }
 }
+
+function mostrarBalao(){
+    var balao = document.getElementById('balao');
+
+    if(balao.style.display === 'none'){
+        balao.style.display = 'block';
+    }
+    else{
+        balao.style.display = 'none';
+    }
+}
+
