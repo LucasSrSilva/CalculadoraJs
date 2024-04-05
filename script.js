ed982535af19c1bb2a0a692d0e42868d7d0f83c1
LucasSrSilva <lucas.srs.2022@gmail.com>
2024-04-05 09:22:27 -0300

    update2

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
 function inserirNum(num){
+    let telaConta = document.getElementById("tela-conta").innerHTML;
     document.getElementById("tela-conta").innerHTML += num;
+    rolarParaDireita();
 }
 
 function inserirOperador(op){
-    let tela_conta = document.getElementById("tela-conta").innerHTML;
+    let telaConta = document.getElementById("tela-conta").innerHTML;
     let operadores = "+-*/."
-    let ultimoCaractere = tela_conta.slice(-1);
+    let ultimoCaractere = telaConta.slice(-1);
 
-    if (operadores.includes(ultimoCaractere) || tela_conta.length === 0){
+    if (operadores.includes(ultimoCaractere) || telaConta.length === 0){
         alert("Operadores só podem ser colocados depois de números");
     }else{
     document.getElementById("tela-conta").innerHTML += op;
+    rolarParaDireita();
     }
 }
 
 function inserirParenteses(){
-    let tela_conta = document.getElementById("tela-conta").innerHTML;
+    let telaConta = document.getElementById("tela-conta").innerHTML;
     let parenteses = "(";
 
-    let abreParenteses = (tela_conta.match(/\(/g) || []).length
-    let fechaParenteses = (tela_conta.match(/\)/g) || []).length
+    let abreParenteses = (telaConta.match(/\(/g) || []).length
+    let fechaParenteses = (telaConta.match(/\)/g) || []).length
 
     if(abreParenteses > fechaParenteses){
         parenteses = ")";
     }
     document.getElementById('tela-conta').innerHTML += parenteses;
+    rolarParaDireita();
 }
 
 function apagar(){
@@ -36,7 +40,12 @@ function limpar(){
 }
 
 function calcular(){
-    tela_conta = document.getElementById('tela-conta').innerHTML;
-    resultado = eval(tela_conta);
+    let telaConta = document.getElementById('tela-conta').innerHTML;
+    resultado = eval(telaConta);
     document.getElementById('resultado').innerHTML = resultado;
+}
+
+function rolarParaDireita() {
+    let telaConta = document.getElementById('tela-conta');
+    telaConta.scrollLeft = telaConta.scrollWidth;
 }
